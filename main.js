e044a807d00d5201ff9d71cd7a3383cc8b0e46de
unknown <uzieloliveira09@gmail.com>
2025-05-27 19:45:31 -0300

    tela de loading

--- main.js
+++ main.js
@@ -1,6 +1,46 @@
+function carregar(){
+    const divCard = document.querySelector(".cardBox")
+    
+    fetch("./dados.json").then((response) => {
+        response.json().then((dados) => {
+            dados.map((dado) => {
+                        divCard.innerHTML += `<div class="card">
+                        <img src="${dado.end_Img}" alt="">
+                        <div class="conteudoCard">
+                            <p>${dado.descricao}</p>
+                            <button> ver mais detalhes></button>
+                        </div>
+                        </div>`
+            })
+        })
+    })
+}
 
+function mostrarLoading(){
+    document.querySelector(".loading-screen").style.display = "flex";
+}
 
-function getData(){
+function esconderLoading(){
+      document.querySelector(".loading-screen").style.display = "none";
+       pesquisa();
+}
+
+function limpar(){
+ 
+    document.querySelector(".cardBox").innerHTML = `<div class='loading-screen'>
+                <div class='spinner'></div>
+                <p>Carregando...</p>
+            </div>`
+
+    mostrarLoading();
+    
+    setTimeout(()=>{
+        esconderLoading();
+    }, 2000);
+
+}
+
+function pesquisa(){
     
     const divCard = document.querySelector(".cardBox")
     const pesquisa = document.querySelector("#search").value
@@ -10,7 +50,6 @@ function getData(){
             dados.map((dado) => {
                   
                     if(dado.tipo == pesquisa){
-
                         divCard.innerHTML += `<div class="card">
                         <img src="${dado.end_Img}" alt="">
                         <div class="conteudoCard">
@@ -23,3 +62,5 @@ function getData(){
         })
     })
 }
+
+carregar();
